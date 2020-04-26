a32b25c49bf6c937f8bffbc2f33b6160c62d3c23
s3645695 <charlotte@charlottesmbp3.modem>
2020-04-26 15:54:39 +1000

    skeleton classes with rough descriptions of how to proceed

---
+++ SEPM Assignment/src/main/Location.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Location
+{
+		// create location: add name, add description, add x-y coordinates, add time spent
+	
+		// edit location
+		
+		// remove location
+		
+		// copy location to the set of other locations
+}
---
+++ SEPM Assignment/src/main/Tour.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Tour
+{
+	// create tour: add name, add duration, add type
+	
+	// remove tour
+	
+	// edit tour: add location to tour, edit tour name, remove location from tour
+	
+	// edit tour type: change type, add new type, remove type
+}
